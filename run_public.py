ae1fe53f7536484150910c9265afdaee64cedfc5
Satvika17-web <satvika.a17@gmail.com>
2026-08-14 19:50:33 +0530

    Final version of EDUMITHRA platform

--- run_public.py
+++ run_public.py
@@ -1,5 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    # This points to the 'app' object inside 'main.py' which is now inside the 'backend' folder
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, reload=False)
